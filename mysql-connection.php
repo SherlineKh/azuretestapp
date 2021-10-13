6f97284db0362b1872d2c7136dda7ce2ec5bc5de
Vipin Yadav <47641456+vipin0@users.noreply.github.com>
2021-10-13 14:46:33 +0530

    added scripts

--- mysql-connection.php
+++ mysql-connection.php
@@ -4,7 +4,7 @@ $database = getenv('DB_NAME');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 // Create connection
-$conn = new mysqli($servername, $username, $password,$database);
+$conn = new mysqli($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
