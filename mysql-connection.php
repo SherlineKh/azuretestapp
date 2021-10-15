e0c1593bd449a2a7c4265308be1b1e4f7de245a6
Vipin Yadav <47641456+vipin0@users.noreply.github.com>
2021-10-15 14:05:00 +0530

    Update mysql-connection.php

--- mysql-connection.php
+++ mysql-connection.php
@@ -6,8 +6,9 @@ $password = getenv('DB_PASSWORD');
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 // Check connection
-if ($conn->connect_error) {
+if (!$conn) {
   die("Connection failed: " . $conn->connect_error);
+}else{
+  echo "Connected successfully";
 }
-echo "Connected successfully";
 ?>
